704a7d3b0dc20fbcf5e0f5081ee75fc3f598fe78
tiffanyGB <tiffany.gbellile@gmail.com>
2023-06-28 10:59:53 +0200

    changement creeation admin par defaut

--- public/javascripts/hachage_admin.js
+++ public/javascripts/hachage_admin.js
@@ -2,51 +2,50 @@ const bcrypt = require('bcrypt');
 const pool = require('../../database/configDB');
 const fmdp = require('../javascripts/index/fonctions_mdp');
 
-
-
 const createDefaultUser = async () => {
     try {
-      const password = 'admin'; // Mot de passe en clair
-  
-      /* Génération du mot de passe */
-      const hashedPassword = await fmdp.salageMdp(password);
-  
-      const query = `
-        INSERT INTO Utilisateur (nom, prenom, pseudo, email, date_inscription, hashMdp, typeUser)
-        VALUES ('admin', 'admin', 'admin', 'admin@admin.fr', CURRENT_DATE, $1, 'administrateur')
-      `;
-  
-      const params = [hashedPassword];
-  
-      await pool.query(query, params);
-  
-      console.log('Utilisateur par défaut créé avec succès');
-    } catch (error) {
-      console.error('Erreur lors de la création de l\'utilisateur par défaut:', error);
-    } finally {
-      pool.end();
-    }
-  };
+        const password = 'admin';
 
-function createDefaultAdmin(){
+        /* Génération du mot de passe */
+        const hashedPassword = await fmdp.salageMdp(password);
+
+        const query = `
+            INSERT INTO Utilisateur (nom, prenom, pseudo, email, date_inscription, hashMdp, typeUser)
+            VALUES ('admin', 'admin', 'admin', 'admin@admin.fr', CURRENT_DATE, $1, 'administrateur')
+        `;
+
+        const params = [hashedPassword];
+
+        await pool.query(query, params);
+
+        console.log('Utilisateur par défaut créé avec succès');
+    } catch (error) {
+        console.error('Erreur lors de la création de l\'utilisateur par défaut:', error);
+    }
+};
+
+const createDefaultAdmin = async () => {
     try {    
         const query = `
-          INSERT INTO Admini ()
-          VALUES (1)
+            INSERT INTO Admini (idAdmin)
+            VALUES (1)
         `;
-    
-    
-        pool.query(query);
-    
+
+        await pool.query(query);
+
         console.log('Admin par défaut créé avec succès');
-      } catch (error) {
+    } catch (error) {
         console.error('Erreur lors de la création de l\'admin par défaut:', error);
-      } finally {
-        pool.end();
-      }
+    }
 }
-  
 
-createDefaultUser();
-// createDefaultAdmin();
-
+(async () => {
+    try {
+        await createDefaultUser();
+        await createDefaultAdmin();
+    } catch (error) {
+        console.error('Erreur lors de la création de l\'utilisateur/admin par défaut:', error);
+    } finally {
+        pool.end();
+    }
+})();
